7b4a780007003b2a1a12cc7df5ca15b269486c52
sqrlmn <sqrlmn@gmail.com>
2021-03-10 14:04:40 -0800

    Update Data Name

--- LibraryAssistantDAL/AccountDAL.cs
+++ LibraryAssistantDAL/AccountDAL.cs
@@ -12,7 +12,7 @@ namespace LibraryAssistantDAL
         public bool LoginDAL(string username, string password)
         {
             MySqlConnection conn = new MySqlConnection("Server=libraryassistant.cwhg663yxudq.us-west-2.rds.amazonaws.com;Database=library;Uid=la583;Pwd=la583password;");
-            MySqlCommand cmd = new MySqlCommand("SELECT accountId, accountPw FROM accounts WHERE accountId=@username and accountPw=@password", conn);
+            MySqlCommand cmd = new MySqlCommand("SELECT username, password FROM accounts WHERE username=@username and password=@password", conn);
             cmd.Parameters.Add(new MySqlParameter("@username", username));
             cmd.Parameters.Add(new MySqlParameter("@password", password));
             conn.Open();
@@ -33,7 +33,7 @@ namespace LibraryAssistantDAL
         public string GetUserFirstNameDAL(string username)
         {
             MySqlConnection conn = new MySqlConnection("Server=libraryassistant.cwhg663yxudq.us-west-2.rds.amazonaws.com;Database=library;Uid=la583;Pwd=la583password;");
-            MySqlCommand cmd = new MySqlCommand("SELECT firstName FROM accounts WHERE accountId=@username", conn);
+            MySqlCommand cmd = new MySqlCommand("SELECT firstName FROM accounts WHERE username=@username", conn);
             cmd.Parameters.Add(new MySqlParameter("@username", username));
             conn.Open();
             string firstName = (string)cmd.ExecuteScalar();
@@ -44,7 +44,7 @@ namespace LibraryAssistantDAL
         public string GetUserLastNameDAL(string username)
         {
             MySqlConnection conn = new MySqlConnection("Server=libraryassistant.cwhg663yxudq.us-west-2.rds.amazonaws.com;Database=library;Uid=la583;Pwd=la583password;");
-            MySqlCommand cmd = new MySqlCommand("SELECT lastName FROM accounts WHERE accountId=@username", conn);
+            MySqlCommand cmd = new MySqlCommand("SELECT lastName FROM accounts WHERE username=@username", conn);
             cmd.Parameters.Add(new MySqlParameter("@username", username));
             conn.Open();
             string lastName = (string)cmd.ExecuteScalar();
@@ -55,12 +55,45 @@ namespace LibraryAssistantDAL
         public bool GetUserTypeDAL(string username)
         {
             MySqlConnection conn = new MySqlConnection("Server=libraryassistant.cwhg663yxudq.us-west-2.rds.amazonaws.com;Database=library;Uid=la583;Pwd=la583password;");
-            MySqlCommand cmd = new MySqlCommand("SELECT type FROM accounts WHERE accountId=@username", conn);
+            MySqlCommand cmd = new MySqlCommand("SELECT type FROM accounts WHERE username=@username", conn);
             cmd.Parameters.Add(new MySqlParameter("@username", username));
             conn.Open();
             bool type = (bool)cmd.ExecuteScalar();
             conn.Close();
             return type;
+        }
+
+        public string GetUserEmailDAL(string username)
+        {
+            MySqlConnection conn = new MySqlConnection("Server=libraryassistant.cwhg663yxudq.us-west-2.rds.amazonaws.com;Database=library;Uid=la583;Pwd=la583password;");
+            MySqlCommand cmd = new MySqlCommand("SELECT emailAddress FROM accounts WHERE username=@username", conn);
+            cmd.Parameters.Add(new MySqlParameter("@username", username));
+            conn.Open();
+            string email = (string)cmd.ExecuteScalar();
+            conn.Close();
+            return email;
+        }
+
+        public string GetUserAddressDAL(string username)
+        {
+            MySqlConnection conn = new MySqlConnection("Server=libraryassistant.cwhg663yxudq.us-west-2.rds.amazonaws.com;Database=library;Uid=la583;Pwd=la583password;");
+            MySqlCommand cmd = new MySqlCommand("SELECT address FROM accounts WHERE username=@username", conn);
+            cmd.Parameters.Add(new MySqlParameter("@username", username));
+            conn.Open();
+            string address = (string)cmd.ExecuteScalar();
+            conn.Close();
+            return address;
+        }
+
+        public string GetUserPhoneDAL(string username)
+        {
+            MySqlConnection conn = new MySqlConnection("Server=libraryassistant.cwhg663yxudq.us-west-2.rds.amazonaws.com;Database=library;Uid=la583;Pwd=la583password;");
+            MySqlCommand cmd = new MySqlCommand("SELECT phoneNumber FROM accounts WHERE username=@username", conn);
+            cmd.Parameters.Add(new MySqlParameter("@username", username));
+            conn.Open();
+            string phone = (string)cmd.ExecuteScalar();
+            conn.Close();
+            return phone;
         }
     }
 }
