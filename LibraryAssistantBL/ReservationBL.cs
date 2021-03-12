a052ae54bcaef21016f999aecf9f07937cc2a0ac
travis <cripe21@gmail.com>
2021-03-12 13:22:32 -0800

    reservation classes

---
+++ LibraryAssistantBL/ReservationBL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryAssistantDAL;
+
+namespace LibraryAssistantBL
+{
+    public class ReservationBL
+    {
+        public string GetReservationBL(string keyword)
+        {
+            ReservationDAL reservationDal = new ReservationDAL();
+            string ds = reservationDal.GetUserReservationIdDAL(keyword);
+            return ds;
+        }
+
+        public bool SetReservationBL(string reservationID, string rUsername, string rISBN)
+        {
+            ReservationDAL reservationDal = new ReservationDAL();
+            bool isDone = reservationDal.setReservationDAL(reservationID, rUsername, rISBN);
+            return isDone;
+        }
+    }
+}
