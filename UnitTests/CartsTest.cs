ba5612cd8a436a78b6fa9380a739e4ed59d20227
cmnguyen01 <46499645+cmnguyen01@users.noreply.github.com>
2021-03-29 10:15:55 -0700

    Create CartsTest.cs

---
+++ UnitTests/CartsTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LibraryAssistantDAL;
+using LibraryAssistantBL;
+using LibraryAssistant;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CartsTest
+    {
+        [TestMethod]
+        public void TestcreateCarts()
+        {
+            CartsDAL cartDal = new CartsDAL();
+            var username = "a";
+            var isbn = "4";
+            var id = 4;
+            var result = cartDal.CreateCartsbyUsername(username,isbn,id);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestRemoveCartbyUsername()
+        {
+            CartsDAL cartDal = new CartsDAL();
+            var username = "dd";
+            var result = cartDal.RemoveCartsbyUsername(username);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestCartisempty()
+        {
+            CartsDAL cartDal = new CartsDAL();
+            var result = cartDal.cartisEmpty();
+            Assert.IsFalse(result);
+
+        }
+
+    }
+}
