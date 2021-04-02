91a6c07c77a06c2df05ac97e62c698388089a792
cmnguyen01 <46499645+cmnguyen01@users.noreply.github.com>
2021-04-01 19:22:48 -0700

    Update BookTest.cs

--- UnitTests/BookTest.cs
+++ UnitTests/BookTest.cs
@@ -10,9 +10,28 @@ namespace UnitTests
     public class BookTest
     {
         [TestMethod]
-        public void TestReturnBook()
+        public void TestBookReturnFalse()
         {
+            BookDAL testbook = new BookDAL();
+            var isbn = "1";
+            var username = "a";
+            var result = testbook.ReturnBookDAL(username, isbn);
+            Assert.IsFalse(result);
+        }
 
+        [TestMethod]
+        public void TestBookReturnTrue()
+        {
+            BorrowsDAL returntest = new BorrowsDAL();
+            Random rnd_id = new Random();
+            int alert_id = rnd_id.Next(1, 1000);
+            returntest.CreateBorrowByUsername(alert_id, "a", "1");
+            returntest.ChangeBorrowStatusByUsername("a");
+            BookDAL testbook = new BookDAL();
+            var isbn = "1";
+            var username = "a";
+            var result = testbook.ReturnBookDAL(username, isbn);
+            Assert.IsTrue(result);
         }
         [TestMethod]
         public void TestAddBook()
@@ -46,9 +65,6 @@ namespace UnitTests
             var result = testbook.RemoveBookByISBNDAL("99");
             Assert.IsTrue(result);
         }
-        
-
-
         [TestMethod]
         public void TestEditBookbyISBN()
         {
@@ -83,6 +99,26 @@ namespace UnitTests
             var result = testbook.GetstarCountDAL(isbn);
             Assert.AreEqual(19, result);
         }
+        [TestMethod]
+        public void Testupdatereviewcount()
+        {
+            BookDAL testbook = new BookDAL();
+            //keep this just in case to test the remove book if the test case used for addbook was deleted
+            var isbn = "99";
+            var title = "alphabet soup";
+            var author = "Marie Holly";
+            var quantity = 2;
+            var price = 1.00;
+            var subject = "education";
+            var pagecount = 200;
+            //var currentISBN=testbook.CheckISBNDAL(isbn)
+            var createbook = testbook.AddBookDAL(isbn, title, author, quantity, price, subject, pagecount);
+            testbook.updateBookreviewDAL(isbn, 1);
+            var rc = testbook.GetReviewCountDAL(isbn);
+            Assert.AreEqual(1, rc);
+            testbook.RemoveBookByISBNDAL(isbn);
+        }
+
 
 
     }
